3c72b369ae015e91a823999467df310fd8a13d0e
jonatasrmoura <jonatas.rmoura@outlook.com>
2021-09-18 19:54:36 -0300

    feat#definicao dos temas de cores

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { ThemeProvider } from 'styled-components';
+
 import GlobalStyles from "./styles/GlobalStyles";
 import { Layout } from "./components/Layout";
+import  dark from './styles/themes/dark';
+// import  light from './styles/themes/light';
 
 const App = () => {
   return (
-    <>
+    <ThemeProvider theme={dark}>
       <GlobalStyles />
       <Layout />
-    </>
+    </ThemeProvider>
   );
 }
 
