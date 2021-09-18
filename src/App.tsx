bba285864b05c7e52c46a00a5fcc4608206917fb
jonatasrmoura <jonatas.rmoura@outlook.com>
2021-09-18 18:32:53 -0300

    feat#Global Styles and fonts

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import GlobalStyles from "./styles/GlobalStyles";
+import { Dashboard } from "./pages/Dashboard";
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <Dashboard />
+    </>
+  );
+}
+
+export { App };
