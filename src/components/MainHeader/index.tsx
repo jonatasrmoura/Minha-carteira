92ae0b1c2674c97073404bd5dac0fac3d820ee5c
jonatasrmoura <jonatas.rmoura@outlook.com>
2021-09-18 21:40:20 -0300

    feat#Emojis em MainHeader

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,9 +1,29 @@
-import { Container } from './styles';
+import React, { useMemo } from 'react';
+
+import {
+  Container,
+  Profile,
+  Welcome,
+  UserName
+} from './styles';
+
+import emojis from '../../utils/emojis';
 
 const MainHeader = () => {
+  const emoji = useMemo(() => {
+    const indice = Math.floor(Math.random() * emojis.length);
+
+    return emojis[indice];
+  }, []);
+
   return (
     <Container>
-      <h1>MainHeader</h1>
+      <h1>Toogle</h1>
+
+      <Profile>
+        <Welcome>Olá, {emoji}</Welcome>
+        <UserName>Jonatas Rosa Moura</UserName>
+      </Profile>
     </Container>
   );
 }
---
+++ src/utils/emojis.ts
@@ -0,0 +1,2 @@
+// Toda vez que á página for atualizada meu programa vai sortiar um emoji desse para ser exibido
+export default ['🤑', '🤩', '😍', '😎', '😄'];
