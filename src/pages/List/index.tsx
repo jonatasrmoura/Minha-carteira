c1d3098e2f4990c54cc7f937b7a11e9a27de28a5
jonatasrmoura <jonatas.rmoura@outlook.com>
2021-09-19 23:14:12 -0300

    feat#Criando os filtros das listagem

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -4,13 +4,23 @@ import {
   HistoryFinanceCard
 } from '../../components';
 
-import { Container, Content } from './styles';
+import {
+  Container,
+  Content,
+  Filters,
+} from './styles';
 
 const List = () => {
-  const options = [
-    {value: 'Jonatas', label: 'Jonatas'},
-    {value: 'Rodrigo', label: 'Rodrigo'},
-    {value: 'Maria', label: 'Maria'},
+  const months = [
+    {value: 9, label: 'Setembro'},
+    {value: 8, label: 'Agosto'},
+    {value: 7, label: 'Julho'},
+  ];
+
+  const years = [
+    {value: 2021, label: 2021},
+    {value: 2020, label: 2020},
+    {value: 2019, label: 2019},
   ];
 
   return (
@@ -19,8 +29,25 @@ const List = () => {
         title="Saídas"
         lineColor="#E44C4E"
       >
-        <SelectInput option={options}/>
+        <SelectInput option={months}/>
+        <SelectInput option={years}/>
       </ContentHeader>
+
+      <Filters>
+        <button
+          type="button"
+          className="tag-filter tag-filter-recurrent"
+        >
+          Recorrentes
+        </button>
+
+        <button
+          type="button"
+          className="tag-filter tag-filter-eventual"
+        >
+          Eventuais
+        </button>
+      </Filters>
 
       <Content>
         <HistoryFinanceCard
